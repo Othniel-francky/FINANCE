6b86b0c1723220e219035604afe25dc8e775a049
othniel francky <kouamefranckolivier45@gmail.com>
2023-09-12 11:52:23 +0000

    matin

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,46 @@
 import React from "react";
-import {SiYourtraveldottv} from "react-icons/si";
+import {TbGridDots} from "react-icons/tb";
+import {AiFillCloseCircle} from "react-icons/ai";
+import '../Style/navbar.css';
 
-
+import imag1 from '../assets/LOGO.png'
 const Navbar = () => {
     return (
         <div>
            <header>
-            
+
+            <div className="logoDiv">
+              <a href="#" className="logo">
+            <img src={imag1} alt="" />SIKA FINANCE
+        </a>
+            </div>
+           
+        <div className="toggleNabar">
+                <TbGridDots className="icon"/>
+                </div>
+
+                <ul className="Navbar">
+                  <li>ACCEUIL</li>
+                  <li>A PROPOS DE NOUS</li>
+                  <li>PRESTATION DE SERVICE</li>
+                  <li>CONTACTS</li>
+                  <li>ETUDE FINANCIERE</li>
+
+                  <div className="headerBtns flex">
+                        <button className="btn loginBtn">
+                            M'inscrire
+                        </button>
+                        <button className="btn">
+                            Mon Espace Touristique
+                        </button>
+                     </div>
+                </ul>
+                <div className="closeNavbar">
+                <AiFillCloseCircle className="icon"/>
+                </div>
+
            </header>
         </div>
     )
 }
-export default Navbar;
+export default Navbar; 
