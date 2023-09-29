01ad72fa9e00d71fdfbfd5112fc90b7cc2204dd2
othniel francky <kouamefranckolivier45@gmail.com>
2023-09-29 09:57:02 +0000

    thui

--- src/pages/Etude/Etude.jsx
+++ src/pages/Etude/Etude.jsx
@@ -5,6 +5,7 @@ import Home from '../../Components/Home'
 import Footer from '../../Components/Footer'
 
 import met from "../../assets/DG.jpeg"
+import mis from "../../assets/Directrice.jpeg"
 const Etude = () => {
   return (
     <div>
@@ -20,7 +21,7 @@ const Etude = () => {
             <div className="container">
               <div className="mets">
                 <div className="tire">
-                  <h1>Pourquoi s'eduquer financiérement</h1>
+                  <h1>Pourquoi s'eduquer financiérement ?</h1>
                 </div>
                 <div className="para">
                   <p>L'éducation financière s'adresse à tous, non seulement aux enfants, mais également aux adultes. S'éduquer financièrement, c'est se donner les moyens de développer des connaissances pour comprendre le monde financier, pour être en mesure de prendre ses propres décisions, en connaissance de cause et en toute confiance. L’éducation financière est non seulement un atout de taille pour apprendre à gérer son argent, se constituer une épargne et investir sur le long terme, mais c’est aussi une arme solide pour contribuer à l’économie.</p>
@@ -39,7 +40,7 @@ const Etude = () => {
               <br /><br />
               <div className="mets">
                 <div className="tire">
-                  <h1>Pourquoi s'eduquer financiérement</h1>
+                  <h1>C'est quoi l'intelligence financière ? ?</h1>
                 </div>
                 <div className="para">
                   <p>L'éducation financière s'adresse à tous, non seulement aux enfants, mais également aux adultes. S'éduquer financièrement, c'est se donner les moyens de développer des connaissances pour comprendre le monde financier, pour être en mesure de prendre ses propres décisions, en connaissance de cause et en toute confiance. L’éducation financière est non seulement un atout de taille pour apprendre à gérer son argent, se constituer une épargne et investir sur le long terme, mais c’est aussi une arme solide pour contribuer à l’économie.</p>
@@ -50,7 +51,7 @@ const Etude = () => {
 
               <div className="mets">
                 <div className="tire">
-                  <h1>Pourquoi s'eduquer financiérement</h1>
+                  <h1>Comment bien réussir dans la vie financierement ?</h1>
                 </div>
                 <div className="para">
                   <p>L'éducation financière s'adresse à tous, non seulement aux enfants, mais également aux adultes. S'éduquer financièrement, c'est se donner les moyens de développer des connaissances pour comprendre le monde financier, pour être en mesure de prendre ses propres décisions, en connaissance de cause et en toute confiance. L’éducation financière est non seulement un atout de taille pour apprendre à gérer son argent, se constituer une épargne et investir sur le long terme, mais c’est aussi une arme solide pour contribuer à l’économie.</p>
@@ -77,9 +78,31 @@ const Etude = () => {
             <button className="bttn">Contacter</button>
           </div>
         </div>
+<br /> <br />
+        <div className="part1">
+          
+          <div className="ecrire">
+            <h1>Marie Constance</h1>
+            <p>
+              Expert Comptable a Boulangère
+            </p>
+            <button className="bttn">Contacter</button>
+          </div>
+
+          <div className="prob">
+            <img src={mis} alt="" />
+          </div>
+        </div>
               </div>
             </div>
 
+<br /><br /><br />
+            <div className="text">
+            <h1>Nos Formations</h1>
+            <div className="depa">
+                <div className="trai"></div>
+            </div>
+            </div>
             <Footer/>
     </div>
   )
